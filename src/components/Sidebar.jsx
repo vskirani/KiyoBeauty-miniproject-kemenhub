4944074a91f4b201fe30eb1c1d3ee1e0260a3944
unknown <kvitriseptia@gmail.com>
2025-12-11 16:00:07 +0700

    update width sidebar mobile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return (
-    <aside className="flex flex-col p-6 fixed left-0 top-0 w-64 h-screen bg-white border-r shadow-sm">
+    <aside className="flex flex-col p-6 fixed left-0 top-0 w-40 h-screen bg-white border-r shadow-sm">
       {/* Logo / Title */}
       <h1 className="text-2xl font-bold mb-10 tracking-tight">Dashboard</h1>
 
